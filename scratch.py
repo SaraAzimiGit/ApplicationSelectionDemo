225bf90aae568de467b34917e792f8289b8d2303
SaraAzimiGit <87041046+SaraAzimiGit@users.noreply.github.com>
2021-09-29 19:59:42 -0400

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -11,8 +11,8 @@ def Convert(string):
     li=list(string.split(",",))
     return li
 
-siteHeader=st.container
-appSelection=st.container
+siteHeader=st.container()
+appSelection=st.container()
 
 
 with siteHeader:
