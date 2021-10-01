2a5c1176d99c071022ec258e0081d3231acab1d3
SaraAzimiGit <87041046+SaraAzimiGit@users.noreply.github.com>
2021-10-01 10:27:14 -0400

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -72,56 +72,56 @@ best suits your facility management application needs""")
             q1 = st.multiselect('Select facility management application(s)', df_1.index)
             Menu_Choices = st.selectbox('Select the options', Menu_Items)
             submit_button = st.form_submit_button(label='Update')
-            if submit_button:
-                result_1 = df_1.loc[q1]
-                app_selected = pd.DataFrame(data=result_1, columns=columns)
-                app_selected.update(df)
+        if submit_button:
+            result_1 = df_1.loc[q1]
+            app_selected = pd.DataFrame(data=result_1, columns=columns)
+            app_selected.update(df)
 
-                max_occ_res = app_selected.Occupancy_Resolution.max()
-                max_spatial_res = app_selected.Spatial_Resolution.max()
-                max_acc = app_selected.Accuracy.max()
+            max_occ_res = app_selected.Occupancy_Resolution.max()
+            max_spatial_res = app_selected.Spatial_Resolution.max()
+            max_acc = app_selected.Accuracy.max()
 
-                min_occ_res = app_selected.Occupancy_Resolution.min()
-                min_spatial_res = app_selected.Spatial_Resolution.min()
-                min_acc = app_selected.Accuracy.min()
+            min_occ_res = app_selected.Occupancy_Resolution.min()
+            min_spatial_res = app_selected.Spatial_Resolution.min()
+            min_acc = app_selected.Accuracy.min()
 
-                if Menu_Choices == "View all sensors available":
-                    st.write("Below are all the sensors Available")
-                    st.dataframe(df1_1)
-                if Menu_Choices == "Select single type of sensor based on application criteria":
-                    filter_table1 = df_0.loc[(df_0['Occupancy_Resolution'] >= max_occ_res) & (df_0['Accuracy'] >= max_acc) & (
-                                df_0['Spatial_Resolution'] >= max_spatial_res)]
-                    filter_table1.update(df1_1)
-                    st.dataframe(filter_table1)
-                if Menu_Choices == "Select combination of sensors based on application criteria":
-                    info_sensor_0 = {'Sensor': ['PIR/Break beam/Ultrasonic/Microwave', 'TOF/Binocular/SL/Infrared camera',
-                                   'Optical camera','Wi-Fi', 'RFID tag/UWB/Bluetooth', 'Acoustic/Smart meters', 'Door/CO2','Piezoelectric'],
-                                    'Occupancy_Resolution': [2, 2, 3, 3, 3, 2, 2, 1],
-                                    'Spatial_Resolution': [4, 4, 4, 3, 3, 3, 3, 4],
-                                    'Accuracy': [1, 3, 3, 3, 3, 2, 1, 1],
-                                    'Privacy': [3, 3, 1, 1, 1, 3, 3, 3],
-                                    'Cost': [1, 3, 3, 1, 2, 1, 1, 1]}
-                    columns_0 = ['Sensor', 'Occupancy_Resolution', 'Spatial_Resolution', 'Accuracy', 'Privacy', 'Cost']
-                    df_0 = pd.DataFrame(data=info_sensor_0, columns=columns_0,index=['PIR/Break beam/Ultrasonic/Microwave',
-                                               'TOF/Binocular/SL/Infrared camera','Optical camera', 'Wi-Fi', 'RFID tag/UWB/Bluetooth',
-                                               'Acoustic/Smart meters','Door/CO2', 'Piezoelectric'])
-                    min_table = df_0.loc[(min_occ_res <= df_0['Occupancy_Resolution']) & (min_acc <= df_0['Accuracy']) & (min_spatial_res <= df_0['Spatial_Resolution'])]
-                    sensor = min_table['Sensor']
-                    min_table = df_0.loc[(min_occ_res <= df_0['Occupancy_Resolution']) & (min_acc <= df_0['Accuracy']) & (min_spatial_res <= df_0['Spatial_Resolution'])]
+            if Menu_Choices == "View all sensors available":
+                st.write("Below are all the sensors Available")
+                st.dataframe(df1_1)
+            if Menu_Choices == "Select single type of sensor based on application criteria":
+                filter_table1 = df_0.loc[(df_0['Occupancy_Resolution'] >= max_occ_res) & (df_0['Accuracy'] >= max_acc) & (
+                            df_0['Spatial_Resolution'] >= max_spatial_res)]
+                filter_table1.update(df1_1)
+                st.dataframe(filter_table1)
+            if Menu_Choices == "Select combination of sensors based on application criteria":
+                info_sensor_0 = {'Sensor': ['PIR/Break beam/Ultrasonic/Microwave', 'TOF/Binocular/SL/Infrared camera',
+                               'Optical camera','Wi-Fi', 'RFID tag/UWB/Bluetooth', 'Acoustic/Smart meters', 'Door/CO2','Piezoelectric'],
+                                'Occupancy_Resolution': [2, 2, 3, 3, 3, 2, 2, 1],
+                                'Spatial_Resolution': [4, 4, 4, 3, 3, 3, 3, 4],
+                                'Accuracy': [1, 3, 3, 3, 3, 2, 1, 1],
+                                'Privacy': [3, 3, 1, 1, 1, 3, 3, 3],
+                                'Cost': [1, 3, 3, 1, 2, 1, 1, 1]}
+                columns_0 = ['Sensor', 'Occupancy_Resolution', 'Spatial_Resolution', 'Accuracy', 'Privacy', 'Cost']
+                df_0 = pd.DataFrame(data=info_sensor_0, columns=columns_0,index=['PIR/Break beam/Ultrasonic/Microwave',
+                                           'TOF/Binocular/SL/Infrared camera','Optical camera', 'Wi-Fi', 'RFID tag/UWB/Bluetooth',
+                                           'Acoustic/Smart meters','Door/CO2', 'Piezoelectric'])
+                min_table = df_0.loc[(min_occ_res <= df_0['Occupancy_Resolution']) & (min_acc <= df_0['Accuracy']) & (min_spatial_res <= df_0['Spatial_Resolution'])]
+                sensor = min_table['Sensor']
+                min_table = df_0.loc[(min_occ_res <= df_0['Occupancy_Resolution']) & (min_acc <= df_0['Accuracy']) & (min_spatial_res <= df_0['Spatial_Resolution'])]
 
-                    for f in range(len(sensor) + 1):  # iterate number of items in combination
-                        if f>=2:
-                            for i in r_subset(sensor, f):  # iterate over every combination
-                                is_ok = False
-                                for key in i:  # iterate over any item in a combination
-                                    multi_row = min_table[min_table['Sensor'].isin(convert(key))]
-                                    value_occ_res = multi_row.Occupancy_Resolution.item()
-                                    value_spatial_res = multi_row.Spatial_Resolution.item()
-                                    value_acc = multi_row.Accuracy.item()
-                                    if (value_occ_res >= max_occ_res) and (value_spatial_res >= max_spatial_res) and (value_acc >= max_acc):  # pick any item in a  combination that meets the criteria
-                                        info = df_0.loc[i, :]
-                                        info = info.drop(columns='Sensor')
-                                        info.update(df1_1)
-                                        st.dataframe(info)
+                for f in range(len(sensor) + 1):  # iterate number of items in combination
+                    if f>=2:
+                        for i in r_subset(sensor, f):  # iterate over every combination
+                            is_ok = False
+                            for key in i:  # iterate over any item in a combination
+                                multi_row = min_table[min_table['Sensor'].isin(convert(key))]
+                                value_occ_res = multi_row.Occupancy_Resolution.item()
+                                value_spatial_res = multi_row.Spatial_Resolution.item()
+                                value_acc = multi_row.Accuracy.item()
+                                if (value_occ_res >= max_occ_res) and (value_spatial_res >= max_spatial_res) and (value_acc >= max_acc):  # pick any item in a  combination that meets the criteria
+                                    info = df_0.loc[i, :]
+                                    info = info.drop(columns='Sensor')
+                                    info.update(df1_1)
+                                    st.dataframe(info)
 if __name__ == "__main__":
     main()
