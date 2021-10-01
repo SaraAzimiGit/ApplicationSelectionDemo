5e625f451d12763133bc1c19305bc56a1aef1e25
SaraAzimiGit <87041046+SaraAzimiGit@users.noreply.github.com>
2021-10-01 14:06:50 -0400

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 from itertools import combinations
-showWarningOnDirectExecution = True
-dataFrameSerialization = "arrow"
 
 @st.cache
 def r_subset(arr, r):
